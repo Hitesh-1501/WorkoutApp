8c352937a1359e271ae8d4a4f360d457d0b1387d
Hitesh <badgujarhitesh1501@gmail.com>
2026-06-26 21:00:17 +0530

    domain: introduce AuthRepository interface contract for business logic decoupling

---
+++ app/src/main/java/com/example/auraFitAI/domain/AuthRepository.kt
@@ -0,0 +1,28 @@
+package com.example.auraFitAI.domain
+
+import com.example.auraFitAI.domain.util.NetworkResult
+
+interface AuthRepository {
+    suspend fun signInWithEmail(
+        email: String,
+        password: String
+    ): NetworkResult<Boolean>
+
+    suspend fun signUpWithEmail(
+        email: String,
+        password: String
+    ): NetworkResult<String>
+    
+    
+    suspend fun saveOnboardingData(
+        uid: String,
+        age: Int,
+        height: Double,
+        weight: Double,
+        fitnessGoal: String
+    ): NetworkResult<Boolean>
+
+    fun isUserLoggedIn(): Boolean
+
+    fun logoutUser()
+}
