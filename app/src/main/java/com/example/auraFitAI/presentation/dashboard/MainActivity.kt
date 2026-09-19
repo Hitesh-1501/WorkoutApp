6423418d9c306a9035fdb7e7aafc43a9c60370ad
Hitesh <badgujarhitesh1501@gmail.com>
2026-09-19 17:07:38 +0530

    implement one time login operation

--- app/src/main/java/com/example/auraFitAI/presentation/dashboard/MainActivity.kt
+++ app/src/main/java/com/example/auraFitAI/presentation/dashboard/MainActivity.kt
@@ -9,7 +9,9 @@ import com.example.auraFitAI.R
 import com.example.auraFitAI.databinding.ActivityMainBinding
 import com.example.auraFitAI.presentation.auth.AuthViewModel
 import com.example.auraFitAI.presentation.onboarding.WelcomeCarouselFragment
+import com.example.auraFitAI.presentation.onboarding.OnboardingFragment
 import com.example.auraFitAI.presentation.home.HomeFragment
+import com.example.auraFitAI.presentation.util.SessionManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,11 +27,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            if(viewModel.checkUserSession()){
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, WelcomeCarouselFragment())
-                    .commit()
-            }else{
+            if (viewModel.checkUserSession()) {
+                val currentUserUid = com.google.firebase.auth.FirebaseAuth.getInstance().currentUser?.uid
+                if (currentUserUid != null && SessionManager.isOnboardingCompleted(this, currentUserUid)) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, HomeFragment())
+                        .commit()
+                } else {
+                    val onboardingFragment = OnboardingFragment().apply {
+                        if (currentUserUid != null) {
+                            arguments = Bundle().apply {
+                                putString("USER_UID_KEY", currentUserUid)
+                            }
+                        }
+                    }
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, onboardingFragment)
+                        .commit()
+                }
+            } else {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, WelcomeCarouselFragment())
                     .commit()
